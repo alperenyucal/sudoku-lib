67b3f3e3491dd12ad1ae57ab4aa9a9b639f1cac0
alperenyucal <alperenyucal@gmail.com>
2020-02-18 01:56:59 +0300

    correct variable names

--- sudoku_lib/sudoku.py
+++ sudoku_lib/sudoku.py
@@ -56,8 +56,8 @@ class Sudoku:
 
         self.type = type
         self.size = size
+        self.sudoku_complete = []
         self.sudoku = []
-        self.finalized = []
         self.region_sequence = self.region_sequence_defaults[size]
         self.regional = []
         self.characters = []
@@ -132,7 +132,7 @@ class Sudoku:
         count = 0
         for i in range(self.size):
             for j in range(self.size):
-                if self.sudoku[i][j] == num:
+                if self.sudoku_complete[i][j] == num:
                     count += 1
         return count
 
@@ -151,57 +151,57 @@ class Sudoku:
         size = self.size
         attempt = 0
         num = 1
-        self.sudoku = [[None for i in range(size)] for j in range(size)]
-        temp = [copy.deepcopy(self.sudoku)]
+        self.sudoku_complete = [[None for i in range(size)] for j in range(size)]
+        temp = [copy.deepcopy(self.sudoku_complete)]
 
         while num <= size:
             for row in range(size):
 
                 valid_cells = [col for col in range(
-                    size) if self.is_valid(self.sudoku, num, row, col)]
+                    size) if self.is_valid(self.sudoku_complete, num, row, col)]
 
                 if len(valid_cells) == 0:
-                    self.sudoku = copy.deepcopy(temp[num-1])
+                    self.sudoku_complete = copy.deepcopy(temp[num-1])
                     break
 
                 col = random.choice(valid_cells)
-                self.sudoku[row][col] = num
+                self.sudoku_complete[row][col] = num
 
             attempt += 1
 
             if attempt == size*3:
                 num -= 1
-                self.sudoku = copy.deepcopy(temp[num-1])
+                self.sudoku_complete = copy.deepcopy(temp[num-1])
                 temp.pop()
             elif attempt == size*5:
                 self.generate_full()
 
             if self.count_number(num) == size:
-                temp.append(copy.deepcopy(self.sudoku))
+                temp.append(copy.deepcopy(self.sudoku_complete))
                 num += 1
 
     def finalize(self):
 
         s = self.size
-        finalized = copy.deepcopy(self.sudoku)
+        sudoku = copy.deepcopy(self.sudoku_complete)
         num = random.randint(s*(s-4), s*(s-2))
         count = 0
 
         while True:
             row = random.randint(0, s-1)
             col = random.randint(0, s-1)
-            if self.is_cell_full(self.sudoku, row, col):
-                finalized[row][col] = None
+            if self.is_cell_full(self.sudoku_complete, row, col):
+                sudoku[row][col] = None
                 count += 1
             if count == num:
                 break
 
-        self.finalized = finalized
+        self.sudoku = sudoku
 
     def backtrack(self):
 
         size = self.size
-        sudoku = copy.deepcopy(self.finalized)
+        sudoku = copy.deepcopy(self.sudoku)
 
         empty_cells = [[i, j, 0] for i in range(size) for j in range(
             size) if not self.is_cell_full(sudoku, i, j)]
@@ -237,6 +237,6 @@ class Sudoku:
             if flag == 0 and num == size+1:
                 return False
 
-        self.finalized = copy.deepcopy(sudoku)
+        self.sudoku = copy.deepcopy(sudoku)
 
         return True
